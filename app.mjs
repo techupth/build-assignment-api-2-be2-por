f5ff544761bfc183943e93e1dc2aaaf71af39209
Por <Poramaporn2540@gmail.com>
2024-10-05 16:32:56 +0700

    ✨feat(assingnments): add assignment read, update and delete end point and catch route not found

--- app.mjs
+++ app.mjs
@@ -1,38 +1,15 @@
-import express from "express";
-import connectionPool from "./utils/lms_db.mjs";
+import express, { json } from 'express';
+import connectionPool from './utils/lms_db.mjs';
 import 'dotenv/config';
-import  { body, validationResult } from "express-validator";
+import validationAssignment from './middleware/createValidation.mjs';
+import handleValidationErrors from './middleware/errorHandling.mjs';
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json()); //middleware to help handling JSON data -- JSON parsing
 
-// Validation middleware for assignment creation
-const validationAssignment = [
-    body('title').notEmpty().withMessage('Title is required'),
-    body('content').notEmpty().withMessage('Content is required'),
-    body('category').notEmpty().withMessage('Category is required'),
-];
-
-// Error handling middleware
-const handleValidationErrors = (req, res, next) => {
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()) {
-        //return  all validation errors
-        return res.status(400).json({
-            message: "Server could not create assignment because there are missing data from client",
-            errors: errors.array().map(err => ({
-                field: err.param,
-                message: err.msg
-            }))
-        });
-    }
-    next();
-}; 
-
-app.post("/assignments", validationAssignment, handleValidationErrors, async (req, res) => {
+app.post('/assignments', validationAssignment, handleValidationErrors, async (req, res) => {
     //access data
     const { title, content, category, length, user_id, status } = req.body;
     const assignmentStatus = status || 'draft';
@@ -78,11 +55,120 @@ app.post("/assignments", validationAssignment, handleValidationErrors, async (re
     return res.status(201).json({
         message: "Created assignment sucessfully"
     });
-});
+})
 
+app.get('/assignments', async (req, res) => {
+    try {
+        const result = await connectionPool.query(`SELECT * FROM assignments`);
+        return res.status(200).json({
+            data: result.rows,
+        });
+    } catch (error) {
+        return res.status(500).json({
+             message: "Server could not find a requested assignment",
+             error: error.message
+        });
+    }
+})
+
+app.get('/assignments/:assignmentId', async (req, res) => {
+    const assignmentIdFromClient = req.params.assignmentId;
+
+    try{
+
+        const result = await connectionPool.query('SELECT * FROM assignments WHERE assignment_id = $1', [assignmentIdFromClient]);
+
+    if(!result.rows[0]) {
+        return res.status(404).json({
+            message: "Server could not find a requested assignment"
+        });
+    }
+    } catch (error) {
+        res.status(500).json({
+            message: "Server could not read assignment because database connection",
+            error: error.message
+        });
+    }
+
+    return res.status(200).json({
+        data: result.rows[0],
+    });
+    
+})
+
+app.put('/assignments/:assignmentId', async (req, res) => {
+    const assignmentIdFromClient = req.params.assignmentId;
+    const { title, content, category, length, status } = req.body;
+
+    try {
+        const result = await connectionPool.query(
+            `
+                UPDATE assignments
+                SET
+                    title = $1,
+                    content = $2,
+                    category = $3,
+                    length = $4,
+                    status = $5,
+                    updated_at = NOW()
+                WHERE 
+                    assignment_id = $6;
+            `, [title, content, category, length, status, assignmentIdFromClient]);
+        
+        if(result.rowCount === 0) {
+            return res.status(404).json({
+                message: "Server could not find a requested assignment",
+                error: error.message
+            });
+        }
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Server could not update assignment because database connection",
+            error: error.message
+        });
+    }
+
+    return res.status(200).json({
+        message: "Updated assignment sucessfully"
+    });
+
+})
+
+app.delete('/assignments/:assignmentId', async (req, res) => {
+    const assignmentIdFromClient = req.params.assignmentId;
+
+    try {
+        const result = connectionPool.query(
+            `
+                DELETE FROM assignments WHERE assignment_id = $1
+            `, [assignmentIdFromClient]);
+        if (result.rowCount === 0) {
+            return res.status(404).json({
+                message: "Server could not find a requested assignment to delete"
+            });
+        }
+    } catch (error) {
+        return res.status(500).json({
+            message: "Server could not delete assignment because database connection",
+            error: error.message
+        });
+    }
+
+    return res.status(200).json({
+        message: "Deleted assignment sucessfully"
+    });
+})
+
+// catch-all for route not found
+app.all('*', (req, res) => {
+    return res.status(404).json({
+        message: "Route not found"
+    });
+})
 
 
 app.listen(port, ()=> {
     console.log(`Server running at port: ${port}`);
-});
+})
 
